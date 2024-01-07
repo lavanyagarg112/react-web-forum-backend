25c86b112d6c3c3b2ac896691c3ac9fa9e316b6e
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-07 19:57:02 +0530

    show post added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,15 @@ class PostsController < ApplicationController
         render json: post.errors, status: :unprocessable_entity
       end
     end
+
+    def show
+        @post = Post.find_by(id: params[:id])
+        if @post
+          render json: @post
+        else
+          render json: { error: "Post not found" }, status: :not_found
+        end
+      end
   
     private
   
