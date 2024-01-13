603dd1ebeb2073cc0beec728cef877f76283a2a5
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-13 19:38:24 +0800

    api issue in deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::API
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def fallback_index_html
+        render file: 'public/index.html'
+      end
+
     protected
 
     def configure_permitted_parameters
