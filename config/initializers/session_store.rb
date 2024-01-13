9a3baedcb367d845b8be04a4eeefdfef818f455e
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-14 00:47:28 +0800

    samesite added

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # config/initializers/session_store.rb
 
-Rails.application.config.session_store :cookie_store, key: '_my_unique_app_session'
+Rails.application.config.session_store :cookie_store, key: '_my_unique_app_session', domain: :all, tld_length: 2, same_site: :none, secure: true
+
