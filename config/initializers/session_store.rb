19f5c6750c8d7f53800c6c37406aeb622a2c7749
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-14 13:33:01 +0800

    subdomain added

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # config/initializers/session_store.rb
 
-Rails.application.config.session_store :cookie_store, key: '_my_unique_app_session', domain: :all, tld_length: 2, same_site: :none, secure: true
+Rails.application.config.session_store :cookie_store, key: '_my_unique_app_session', domain: ".onrender.com", tld_length: 2, same_site: :none, secure: true
 
