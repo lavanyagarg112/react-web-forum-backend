05044428ef16d4d409c882c0ec53dad2d5ecc610
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-21 14:14:14 +0800

    authentication token in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,23 @@ class ApplicationController < ActionController::API
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
         devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
+
+    private
+
+    def authenticate_token
+        # Extract the token from the request header
+        token = request.headers['Authorization']&.split(' ')&.last
+    
+        if token
+          begin
+            decoded_token = JWT.decode(token, ENV['SECRET_KEY_BASE'], true, { algorithm: 'HS256' })
+            @current_user = User.find(decoded_token[0]['user_id'])
+          rescue JWT::DecodeError
+            # Handle the case where the token is invalid
+            @current_user = nil
+          end
+        else
+          @current_user = nil
+        end
+      end
 end
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -11,22 +11,6 @@ class AuthenticationController < ApplicationController
     end
   end
 
-  private
 
-  def authenticate_token
-    # Extract the token from the request header
-    token = request.headers['Authorization']&.split(' ')&.last
-
-    if token
-      begin
-        decoded_token = JWT.decode(token, ENV['SECRET_KEY_BASE'], true, { algorithm: 'HS256' })
-        @current_user = User.find(decoded_token[0]['user_id'])
-      rescue JWT::DecodeError
-        # Handle the case where the token is invalid
-        @current_user = nil
-      end
-    else
-      @current_user = nil
-    end
-  end
+  
 end
