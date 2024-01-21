facc800ce48ab64f04ceb1a45b1990e9246ec8ce
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-21 15:24:18 +0800

    post id removed from tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_12_064141) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_21_072308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,10 +58,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_12_064141) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
-    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_tags_on_post_id"
   end
 
   create_table "user_data", force: :cascade do |t|
@@ -94,6 +92,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_12_064141) do
   add_foreign_key "posts", "users"
   add_foreign_key "taggings", "posts"
   add_foreign_key "taggings", "tags"
-  add_foreign_key "tags", "posts"
   add_foreign_key "user_data", "users"
 end
