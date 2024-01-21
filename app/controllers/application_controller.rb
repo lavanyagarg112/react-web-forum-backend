167ebcaefa6614cd0a5b18c1fea9a90338a6072f
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-21 12:03:16 +0800

    revert to non deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,6 @@ class ApplicationController < ActionController::API
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    # def fallback_index_html
-    #     render file: 'public/index.html'
-    #   end
-
     protected
 
     def configure_permitted_parameters
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # config/initializers/session_store.rb
 
-Rails.application.config.session_store :cookie_store, key: '_my_unique_app_session', domain: ".onrender.com", tld_length: 2, same_site: :none, secure: true
-
+Rails.application.config.session_store :cookie_store, key: '_my_unique_app_session'
